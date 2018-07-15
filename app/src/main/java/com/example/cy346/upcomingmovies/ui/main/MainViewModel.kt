823e5a08bffa68cb5ec14f18e4c0c99a37e0edce
Stian Vestengen Beck <cy346@rikstv.no>
2018-07-15 21:58:09 +0200

    basic recycler view, retrofit call with empty respons

---
+++ app/src/main/java/com/example/cy346/upcomingmovies/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.cy346.upcomingmovies.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/cy346/upcomingmovies/ui/main/TextFragment.kt
@@ -0,0 +1,14 @@
+package com.example.cy346.upcomingmovies.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.cy346.upcomingmovies.R
+
+class TextFragment : Fragment(){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.recycler_view, container, false)
+    }
+}
